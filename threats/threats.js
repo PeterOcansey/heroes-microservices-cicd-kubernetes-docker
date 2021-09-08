7a90ea37f4d0964d36c578cf514da9bfbe35845b
peter <ocanseypeter@gmail.com>
2021-09-08 10:25:34 +0000

    feat: added threats service build and linting

--- threats/threats.js
+++ threats/threats.js
@@ -12,26 +12,26 @@ const heroesService = 'http://localhost:8081';
 
 const threats = [
   {
-      id: 1,
-      displayName: 'Pisa tower is about to collapse.',
-      necessaryPowers: ['flying'],
-      img: 'tower.jpg',
-      assignedHero: 0
+    id: 1,
+    displayName: 'Pisa tower is about to collapse.',
+    necessaryPowers: ['flying'],
+    img: 'tower.jpg',
+    assignedHero: 0,
   },
   {
-      id: 2,
-      displayName: 'Engineer is going to clean up server-room.',
-      necessaryPowers: ['teleporting'],
-      img: 'mess.jpg',
-      assignedHero: 0
+    id: 2,
+    displayName: 'Engineer is going to clean up server-room.',
+    necessaryPowers: ['teleporting'],
+    img: 'mess.jpg',
+    assignedHero: 0,
   },
   {
-      id: 3,
-      displayName: 'John will not understand the joke',
-      necessaryPowers: ['clairvoyance'],
-      img: 'joke.jpg',
-      assignedHero: 0
-  }
+    id: 3,
+    displayName: 'John will not understand the joke',
+    necessaryPowers: ['clairvoyance'],
+    img: 'joke.jpg',
+    assignedHero: 0,
+  },
 ];
 
 app.get('/threats', (req, res) => {
@@ -41,24 +41,24 @@ app.get('/threats', (req, res) => {
 
 app.post('/assignment', (req, res) => {
   request.post({
-      headers: {'content-type': 'application/json'},
-      url: `${heroesService}/hero/${req.body.heroId}`,
-      body: `{
-          "busy": true
-      }`
+    headers: { 'content-type': 'application/json' },
+    url: `${heroesService}/hero/${req.body.heroId}`,
+    body: `{
+        "busy": true
+    }`,
   }, (err, heroResponse, body) => {
-      if (!err) {
-          const threatId = parseInt(req.body.threatId);
-          const threat = threats.find(subject => subject.id === threatId);
-          threat.assignedHero = req.body.heroId;
-          res.status(202).send(threat);
-      } else {
-          res.status(400).send({problem: `Hero Service responded with issue ${err}`});
-      }
+    if (!err) {
+      const threatId = parseInt(req.body.threatId, 10);
+      const threat = threats.find((subject) => subject.id === threatId);
+      threat.assignedHero = req.body.heroId;
+      res.status(202).send(threat);
+    } else {
+      res.status(400).send({ problem: `Hero Service responded with issue ${err}` });
+    }
   });
 });
 
-app.use('/img', express.static(path.join(__dirname,'img')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
 
 console.log(`Threats service listening on port ${port}`);
 app.listen(port);
